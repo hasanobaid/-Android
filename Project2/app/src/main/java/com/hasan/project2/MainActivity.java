d7a547158c596e2198a83f76f1978f659e236053
hasanobaid <hasan.kh.obaid@gmail.com>
2019-04-20 21:15:02 +0300

    done done

--- Project2/app/src/main/java/com/hasan/project2/MainActivity.java
+++ Project2/app/src/main/java/com/hasan/project2/MainActivity.java
@@ -1,28 +1,23 @@
 package com.hasan.project2;
 
 import android.content.Intent;
-import android.graphics.Movie;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.hasan.project2.Modle.MovieList;
 import com.hasan.project2.Modle.MovieModle;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -140,4 +135,37 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("START", "I'm in on start method");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("STOP", "I'm in on Stop method");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("PAUSE", "I'm in on Pause method");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Resume", "I'm in on Resume method");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("RESTART", "I'm in on Restart method");
+
+    }
 }
